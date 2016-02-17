3bda61a357e6565e7d6f6f40bfa07e94cd686f8b
Rawd <rpzzzzzz@hotmail.com>
2016-02-17 23:32:49 +0700

    Initial Commit

---
+++ BuildConfigurations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BuildConfigurations
+//
+//  Created by Suraphan on 2/17/2559 BE.
+//  Copyright © 2559 irawd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
